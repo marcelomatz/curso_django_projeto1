f78c6c934243481be9ca096e7f07833c0e1435fd
UmExDev <matzembacher@icloud.com>
2022-11-28 21:34:40 -0300

    removing tests and trashs to start project

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'global/home.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'global/home.html', context={
+        'name': 'Marcelo',
+        'lastname': 'Matzembacher'
+    })
